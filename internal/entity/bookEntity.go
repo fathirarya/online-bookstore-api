23e971a18c72213b2fd80faf5b2f03c9d9e3e012
fathirarya <fathirarya2002@gmail.com>
2025-08-28 13:55:25 +0700

    migration database

--- internal/entity/bookEntity.go
+++ internal/entity/bookEntity.go
@@ -1,13 +1,17 @@
 package entity
 
+import "time"
+
 type Book struct {
-	ID          int     `gorm:"column:id;primaryKey;autoIncrement"`
-	Title       string  `gorm:"column:title;size:255;not null"`
-	Author      string  `gorm:"column:author;size:100;not null"`
-	Price       float64 `gorm:"column:price;type:decimal(10,2);not null"`
-	Year        int     `gorm:"column:year"`
-	CategoryID  int     `gorm:"column:category_id;not null"`
-	ImageBase64 string  `gorm:"column:image_base64;type:text"`
+	ID          int       `gorm:"column:id;primaryKey;autoIncrement"`
+	Title       string    `gorm:"column:title;size:255;not null"`
+	Author      string    `gorm:"column:author;size:100;not null"`
+	Price       float64   `gorm:"column:price;type:decimal(10,2);not null"`
+	Year        int       `gorm:"column:year"`
+	CategoryID  int       `gorm:"column:category_id;not null"`
+	ImageBase64 string    `gorm:"column:image_base64;type:text"`
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdateAt    time.Time `gorm:"column:updated_at;autoUpdateTime"`
 
 	// Relations
 	Category   Category    `gorm:"foreignKey:CategoryID;references:ID"`
--- internal/entity/categoryEntity.go
+++ internal/entity/categoryEntity.go
@@ -1,8 +1,12 @@
 package entity
 
+import "time"
+
 type Category struct {
-	ID   int    `gorm:"column:id;primaryKey;autoIncrement"`
-	Name string `gorm:"column:name;size:100;not null"`
+	ID        int       `gorm:"column:id;primaryKey;autoIncrement"`
+	Name      string    `gorm:"column:name;size:100;not null"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdateAt  time.Time `gorm:"column:updated_at;autoUpdateTime"`
 
 	// Relations
 	Books []Book `gorm:"foreignKey:CategoryID;references:ID"`
